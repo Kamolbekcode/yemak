d67a90ec76c49f332f9064d1a31d69a4dc63aac0
kamolbek_coder <151856865+Kamolbekcode@users.noreply.github.com>
2024-10-22 05:35:49 -0700

    yemak

---
+++ yemak-syrem-system/src/main/java/it/revo/yemak_syrem_system/YemakSyremSystemApplication.java
@@ -0,0 +1,13 @@
+package it.revo.yemak_syrem_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YemakSyremSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YemakSyremSystemApplication.class, args);
+	}
+
+}
---
+++ yemak-syrem-system/src/test/java/it/revo/yemak_syrem_system/YemakSyremSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package it.revo.yemak_syrem_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemakSyremSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
