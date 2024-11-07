9eb7e6aac6e94cf7cee6260d99eb051eee981f2f
kamolbek_coder <151856865+Kamolbekcode@users.noreply.github.com>
2024-11-07 17:10:30 +0500

    18

--- yemak-user-panel/src/component/Header.jsx
+++ yemak-user-panel/src/component/Header.jsx
@@ -4,11 +4,11 @@ export const Header = () => {
   return(
       <div className={'head'}>
           <div className={"head-search"}>
-              <h1 className={"head-logo text-light"}>yemak</h1>
+              <h1 className={"head-logo"}>yemak</h1>
               <input type={"search"} placeholder={"Restoran yoki taom nomi..."} className={"head-search-input"}/>
           </div>
           <div className={"head-location"}>
-              <button className={"btn-1 btn btn-warning text-black"}><i className={"pi pi-tiktok"}></i></button>
+              <button className={"btn-1 btn btn-warning text-black"}><i className={"pi pi-map"}></i></button>
               <button className={"btn-c btn btn-warning text-black"}><a>Kirish</a></button>
           </div>
       </div>
