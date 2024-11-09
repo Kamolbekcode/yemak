b716341c41a715f9c91287d31d7bf6d86a3370c8
skromn1y879 <temurbekboxodirov007@gmail.com>
2024-11-09 14:54:03 +0500

    24

--- yemak-user-panel/src/pages/Menu.jsx
+++ yemak-user-panel/src/pages/Menu.jsx
@@ -1,9 +1,11 @@
-import {Header} from "../../../yemak-user-panel/component/Header.jsx";
+import {Header} from "../component/Header.jsx";
+import {MultiCarousel} from "../component/MultiCarousel.jsx";
 
 export const Menu = () =>{
     return(
         <div>
             <Header/>
+            <MultiCarousel/>
         </div>
     )
 }
